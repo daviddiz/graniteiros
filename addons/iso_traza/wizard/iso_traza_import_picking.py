c969aa9465ba941fde46c5e0af6277c9d96574b5
daviddiz <daviddiz@gmail.com>
2015-02-05 13:49:55 +0100

    Datos de demostración y más ajustes y mejoras

--- addons/iso_traza/wizard/iso_traza_import_picking.py
+++ addons/iso_traza/wizard/iso_traza_import_picking.py
@@ -24,6 +24,7 @@ import os
 import base64
 import tempfile
 from xml.etree import ElementTree
+import datetime
 
 class iso_traza_import_picking(osv.osv_memory):
     _name = 'iso.traza.import.picking'
@@ -38,6 +39,7 @@ class iso_traza_import_picking(osv.osv_memory):
     def import_picking(self, cr, uid, ids, context=None):
         if not context: context = {}
         
+        datetime_now = datetime.datetime.today()
         content = self.browse(cr,uid,ids)[0].file
         
         file_path = tempfile.gettempdir()+'/file.xml'
@@ -70,23 +72,114 @@ class iso_traza_import_picking(osv.osv_memory):
                     Length = node[i].text
                 elif node[i].tag == "UnitOfMeasure":
                     UnitOfMeasure = node[i].text
+                    if UnitOfMeasure == 'KGM':
+                        uom = 2
+                    elif UnitOfMeasure == 'MTR':
+                        uom = 7
+                    else:
+                        uom = 1
                 elif node[i].tag == "NEW":
                     NEW = node[i].text
+                    
+            if sid[0]=="I":
+                product_ids = self.pool.get('product.product').search(cr, uid, [('name_template', '=', ProducerProductName)], context=context)
+                if len(product_ids) > 0 :
+                    sid_existente = self.pool.get('product.product').browse(cr, uid, product_ids[0], context=context).default_code
+                    self.pool.get('product.product').write(cr, uid, product_ids[0], {'default_code': sid_existente+" "+sid})
+                else:        
+                    p_template_vals = {
+                        'supply_method': 'buy',
+                        'standard_price': 1.00,
+                        'mes_type': 'fixed',
+                        'uom_id': uom,
+                        'uom_po_id': uom,
+                        'name': ProducerProductName,
+                        'description': ProducerProductName,
+                        'description_purchase': ProducerProductName,
+                        'description_sale': ProducerProductName,
+                        'type': 'consu',
+                        'procure_method': 'make_to_stock',
+                        'categ_id': 1,
+                        'cost_method': 'standard',
+                        'warranty': 0,
+                        'purchase_ok': True,
+                        'company_id': 1,
+                        'rental': False,
+                        'sale_ok': True,
+                        'sale_delay': 7,
+                        'produce_delay': 1,
+                    }
+                    
+                    product_template_id = self.pool.get('product.template').create(cr, uid, p_template_vals)
+                     
+                    p_product_vals = {
+                        'product_tmpl_id': product_template_id,
+                        'default_code': sid,
+                        'valuation': 'manual_periodic',
+                        'lot_split_type': 'single',
+                        'price_extra': 0.00,
+                        'name_template': ProducerProductName,
+                        'active': True,
+                        'price_margin': 1.00,
+                        'track_production': False,
+                        'track_outgoing': False,
+                        'track_incoming': True,
+                    }
+                    
+                    product_product_id = self.pool.get('product.product').create(cr, uid, p_product_vals)
             
         f.close()
         
         f = open(file_path,'r')
         tree = ElementTree.parse(f)
+         
+        picking_vals = {
+                'type': 'in',
+                'move_type': 'direct',
+                'company_id': 1,
+                'invoice_state': 'none',
+                'auto_picking': False,
+                'location_id': 8,
+                'location_dest_id': 12,
+                'state': 'draft',
+                'date': datetime_now,
+                }
+        picking_id = self.pool.get('stock.picking').create(cr, uid, picking_vals)
         
+        print "alta de albaran"
+         
+        i = 0
         for node in tree.iter('Item'):
-            
-            uid = node.attrib.get('UID')
+            print i
+            i+=1
+             
+            uid_source = node.attrib.get('UID')
             sid = node.attrib.get('SID')
             psn = node.attrib.get('PSN')
             
+            product_ids = self.pool.get('product.product').search(cr, uid, [], context=context)
+            for product_id in product_ids:
+                sid_varios = self.pool.get('product.product').browse(cr, uid, product_id, context=context).default_code.split()
+                if sid in sid_varios:
+                    p_id = product_id
+                    break
+                
+ 
+            move_vals = {
+                     'location_id': 8,
+                     'location_dest_id': 12,
+                     'product_id': p_id,
+                     'picking_id': picking_id,
+                     'product_uom': 1,
+                     'product_uos_qty': 1,
+                     'product_qty': 1,
+                     'name': uid_source,
+                     }    
+            move_id = self.pool.get('stock.move').create(cr, uid, move_vals)  
+             
         f.close()
-        
-        os.remove(f)
+         
+#         os.remove(f)
         
 
 
