66177b044ad8f54e9b6c3d76f134104a21ac511b
daviddiz <daviddiz@gmail.com>
2015-06-17 03:11:46 +0200

    informe acta de consumo

--- addons/iso_traza/report/acta.py
+++ addons/iso_traza/report/acta.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 import time
-
+import pooler
 from report import report_sxw
 
 class acta(report_sxw.rml_parse):
@@ -28,7 +28,47 @@ class acta(report_sxw.rml_parse):
         super(acta, self).__init__(cr, uid, name, context=context)
         self.localcontext.update({
             'time': time,
+            'process':self.process,
         })
+        
+    def process(self,moves_ids):
+#         acta_obj = pooler.get_pool(self.cr.dbname).get('iso.traza.acta')
+#         move_obj = pooler.get_pool(self.cr.dbname).get('stock.move')
+#         moves_id = move_obj.search(self.cr,self.uid, [('acta_id', '=', acta_id)])
+
+        movimientos = []
+        for move_id in moves_ids:
+            m = {}
+            m['nombre'] = move_id.product_id.name
+            m['serial'] = move_id.serial
+            m['cantidad'] = move_id.product_qty
+            movimientos.append(m)
+        movimientos.sort(key=lambda d:(d['nombre'],d['serial']))
+
+        nombre = ""
+        lineas = []
+        data = {}
+        for movimiento in movimientos:
+            if nombre <> movimiento['nombre']:
+                if data:
+                    lineas.append(data)
+                nombre = movimiento['nombre']
+                data['nombre'] = movimiento['nombre']
+                data['serial'] = movimiento['serial']
+                data['entregado'] = 0.0
+                data['sobrante'] = 0.0
+                if movimiento['cantidad']>0:
+                    data['entregado'] = movimiento['cantidad']
+                else:
+                    data['sobrante'] = abs(movimiento['cantidad'])
+            else:
+                data['serial'] = data['serial'] + "\n" + movimiento['serial']
+                if movimiento['cantidad']>0:
+                    data['entregado'] = data['entregado'] + movimiento['cantidad']
+                else:
+                    data['sobrante'] = data['sobrante'] + abs(movimiento['cantidad'])
+        lineas.append(data)
+        return lineas
 
 report_sxw.report_sxw('report.acta', 'iso.traza.acta', 'addons/iso_traza/report/acta.rml', parser=acta, header=False)
 
