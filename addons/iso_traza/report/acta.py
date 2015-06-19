423f5c2213c6230ba019ff558e3187a3645e6cb0
daviddiz <daviddiz@gmail.com>
2015-06-19 14:02:10 +0200

    v0.18 Informe Actas de Consumo

--- addons/iso_traza/report/acta.py
+++ addons/iso_traza/report/acta.py
@@ -42,31 +42,62 @@ class acta(report_sxw.rml_parse):
             m['nombre'] = move_id.product_id.name
             m['serial'] = move_id.serial
             m['cantidad'] = move_id.product_qty
+            m['unidad'] = move_id.product_uom.name
             movimientos.append(m)
         movimientos.sort(key=lambda d:(d['nombre'],d['serial']))
 
         nombre = ""
+        aux = 0
+        serial_anterior = 0
         lineas = []
         data = {}
         for movimiento in movimientos:
             if nombre <> movimiento['nombre']:
                 if data:
+                    if aux==2 and serial_anterior:
+                        data['serial'] = data['serial'] + "\n" + serial_anterior
                     lineas.append(data)
+                    data = {}
+                aux = 0
                 nombre = movimiento['nombre']
                 data['nombre'] = movimiento['nombre']
+                data['unidad'] = movimiento['unidad']    
                 data['serial'] = movimiento['serial']
+                serial_anterior = movimiento['serial']
                 data['entregado'] = 0.0
                 data['sobrante'] = 0.0
                 if movimiento['cantidad']>0:
                     data['entregado'] = movimiento['cantidad']
                 else:
                     data['sobrante'] = abs(movimiento['cantidad'])
+                data['consumido'] = data['entregado'] - data['sobrante']  
+                data['consumido'] = abs(data['consumido'])
             else:
-                data['serial'] = data['serial'] + "\n" + movimiento['serial']
+                if int(movimiento['serial'][-6:]) == ( int(serial_anterior[-6:]) + 1 ):
+                    if aux==0:
+                        data['serial'] = data['serial'] + "\n" + "          al"
+                        aux = 2
+                        serial_anterior = movimiento['serial']
+                    elif aux>0:
+                        serial_anterior = movimiento['serial']
+                        aux = 2
+                else:
+                    if aux>0:
+                        data['serial'] = data['serial'] + "\n" + serial_anterior + "\n" + "    ----------------" + "\n" + movimiento['serial']
+                        serial_anterior = movimiento['serial']
+                        aux = 0
+                    else:
+                        data['serial'] = data['serial'] + "\n" + movimiento['serial']
+                        serial_anterior = movimiento['serial']
+                        aux = 0
                 if movimiento['cantidad']>0:
                     data['entregado'] = data['entregado'] + movimiento['cantidad']
                 else:
                     data['sobrante'] = data['sobrante'] + abs(movimiento['cantidad'])
+                data['consumido'] = data['entregado'] - data['sobrante']
+                data['consumido'] = abs(data['consumido'])
+        if aux==2 and serial_anterior:
+            data['serial'] = data['serial'] + "\n" + serial_anterior
         lineas.append(data)
         return lineas
 
